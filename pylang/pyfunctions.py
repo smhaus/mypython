acc271fed9c28775550f79efab54a3fbcb4f6552
smhaus <smhaus@smhaus-win7>
2017-05-04 23:27:48 -0400

    project files

---
+++ pylang/pyfunctions.py
@@ -0,0 +1,21 @@
+'''
+Created on Jun 9, 2016
+
+@author: smhaus
+'''
+
+def a():
+    print ("a")
+    
+def b():
+    print ("b")
+    
+def two(c,d,e):
+    c()
+    d()
+    e()
+
+
+if __name__ == '__main__':
+    pass
+    two(b,a,b)
